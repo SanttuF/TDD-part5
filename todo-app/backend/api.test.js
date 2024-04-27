7c368c94094dfbc6d4141957d91dc3119ca7dc28
SanttuF <santtu.f@gmail.com>
2024-04-27 17:24:45 +0300

    api

--- todo-app/backend/api.test.js
+++ todo-app/backend/api.test.js
@@ -3,7 +3,20 @@ const app = require('./app')
 
 const api = superTest(app)
 
+const sqlite3 = require('sqlite3').verbose()
+let db =  new sqlite3.Database('./database/db.sqlite')
+db.serialize()
+
 test('getting all todos', async () => {
+  const todo = 'Test123'
+  const todo2 = 'test2'
+  await new Promise((resolve, reject) => {
+    db.run('INSERT INTO todos (todo) VALUES (?)', todo2, (e) => {if(e) reject(e)})
+    db.run('INSERT INTO todos (todo) VALUES (?)', todo, (e) => {
+      if(e) reject(e)
+      resolve(todo)
+    })
+  })
   const res = await api
     .get('/')
     .expect(200)
